c1bb0a18a5961ed90a5e3ee390aa42b6ac89eecb
Xackery Xtal <xackery@gmail.com>
2022-05-31 05:59:23 -0700

    Added gator meat

--- drop/1/oasis_lte.sql
+++ drop/1/oasis_lte.sql
@@ -1,1 +1,7 @@
 # INSERT INTO loottable_entries(probability, loottable_id, lootdrop_id, drop_limit, mindrop) VALUES (100, :todo_lt:, :todo_ld:, 1, 1);
+
+INSERT INTO loottable_entries(probability, loottable_id, lootdrop_id, drop_limit, mindrop) VALUES 
+(50, :oasis_a_deepwater_caiman_lt:, :gator_meat_ld:),
+(50, :oasis_a_caiman_lt:, :gator_meat_ld:),
+(50, :oasis_a_deepwater_crocodile_lt:, :gator_meat_ld:),
+(10, :oasis_lockjaw_lt:, :gator_meat_ld:),
--- drop/13/innothuleb_lte.sql
+++ drop/13/innothuleb_lte.sql
@@ -1,1 +1,4 @@
 # INSERT INTO loottable_entries(probability, loottable_id, lootdrop_id, drop_limit, mindrop) VALUES (100, :todo_lt:, :todo_ld:, 1, 1);
+
+INSERT INTO loottable_entries(probability, loottable_id, lootdrop_id, drop_limit, mindrop) VALUES 
+(30, :innothuleb_a_swamp_alligator_lt:, :gator_meat_ld:),

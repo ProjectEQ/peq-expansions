a1c22c270d894e4b65e42c5f6ba6412e37d1dc68
Xackery Xtal <xackery@gmail.com>
2022-06-03 01:27:51 -0700

    Moved Loam and Withered Leather

---
+++ drop/shared/loam_encrusted_armor.sql
@@ -0,0 +1,16 @@
+INSERT INTO lootdrop(name, min_expansion) VALUES ('loam_encrusted_armor', -1); #:loam_encrusted_armor_ld:
+
+INSERT INTO lootdrop_entries(lootdrop_id, item_id, chance) VALUES
+
+(:loam_encrusted_armor_ld:, 1641, 100); # Loam Encrusted Cap
+(:loam_encrusted_armor_ld:, 1642, 100); # Loam Encrusted Veil
+(:loam_encrusted_armor_ld:, 1643, 100); # Loam Encrusted Choker
+(:loam_encrusted_armor_ld:, 1644, 100); # Loam Encrusted Robe
+(:loam_encrusted_armor_ld:, 1645, 100); # Loam Encrusted Amice
+(:loam_encrusted_armor_ld:, 1646, 100); # Loam Encrusted Cloak
+(:loam_encrusted_armor_ld:, 1647, 100); # Loam Encrusted Sash
+(:loam_encrusted_armor_ld:, 1648, 100); # Loam Encrusted Sleeves
+(:loam_encrusted_armor_ld:, 1649, 100); # Loam Encrusted Bracelet
+(:loam_encrusted_armor_ld:, 1650, 100); # Loam Encrusted Gloves
+(:loam_encrusted_armor_ld:, 1651, 100); # Loam Encrusted Pantaloons
+(:loam_encrusted_armor_ld:, 1652, 100); # Loam Encrusted Lined Shoes
---
+++ drop/shared/withered_leather_armor.sql
@@ -0,0 +1,13 @@
+INSERT INTO lootdrop(name, min_expansion) VALUES ('withered_leather_armor', -1); #:withered_leather_armor_ld:
+
+INSERT INTO lootdrop_entries(lootdrop_id, item_id, chance) VALUES
+
+(:withered_leather_armor_ld:, 2285, 100), # Withered Leather Tunic
+(:withered_leather_armor_ld:, 2287, 100), # Withered Leather Cloak
+(:withered_leather_armor_ld:, 2289, 100), # Withered Leather Sleeves
+(:withered_leather_armor_ld:, 2290, 100), # Withered Leather Wristbands
+(:withered_leather_armor_ld:, 2291, 100), # Withered Leather Gloves
+(:withered_leather_armor_ld:, 2292, 100), # Withered Leather Leggings
+(:withered_leather_armor_ld:, 2294, 100), # Withered Leather Skullcap
+(:withered_leather_armor_ld:, 2295, 100), # Withered Leather Mask
+(:withered_leather_armor_ld:, 2296, 100), # Withered Leather Gorget

295872d5c1369d3f2330ebb23db1396fbb6b0e49
Xackery Xtal <xackery@gmail.com>
2022-06-03 12:33:17 -0700

    Moved Damask

---
+++ drop/shared/damask.sql
@@ -0,0 +1,10 @@
+INSERT INTO lootdrop(name, min_expansion) VALUES ('damask', -1); #:damask_ld:
+(:damask_ld:, 1331, 100); # Damask Cap
+(:damask_ld:, 1332, 100); # Damask Veil
+(:damask_ld:, 1334, 100); # Damask Robe
+(:damask_ld:, 1336, 100); # Damask Cape
+(:damask_ld:, 1337, 100); # Damask Sash
+(:damask_ld:, 1338, 100); # Damask Sleeves
+(:damask_ld:, 1339, 100); # Damask Wristbands
+(:damask_ld:, 1340, 100); # Damask Gloves
+(:damask_ld:, 1341, 100); # Damask Leggings

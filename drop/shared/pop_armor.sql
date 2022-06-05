ab029713211164ed8ea78ee0a1b8be9e1019b7e0
Xackery Xtal <xackery@gmail.com>
2022-06-05 12:51:30 -0700

    Moved pop armor

---
+++ drop/shared/pop_armor.sql
@@ -0,0 +1,112 @@
+
+INSERT INTO lootdrop(name) VALUES('elemental_chain_coif_pattern') # :elemental_chain_coif_pattern_ld:
+INSERT INTO lootdrop_entries(lootdrop_id, item_id, probability) VALUES
+(:elemental_chain_coif_pattern_ld:, 16362, 100); # Elemental Chain Coif Pattern
+
+INSERT INTO lootdrop(name) VALUES('elemental_chain_sleeve_pattern') # :elemental_chain_sleeve_pattern_ld:
+INSERT INTO lootdrop_entries(lootdrop_id, item_id, probability) VALUES
+(:elemental_chain_sleeve_pattern_ld:, 16364, 100); # Elemental Chain Sleeve Pattern
+
+INSERT INTO lootdrop(name) VALUES('elemental_chain_bracelet_pattern') # :elemental_chain_bracelet_pattern_ld:
+INSERT INTO lootdrop_entries(lootdrop_id, item_id, probability) VALUES
+(:elemental_chain_bracelet_pattern_ld:, 16365, 100); # Elemental Chain Bracelet Pattern
+
+INSERT INTO lootdrop(name) VALUES('elemental_chain_glove_pattern') # :elemental_chain_glove_pattern_ld:
+INSERT INTO lootdrop_entries(lootdrop_id, item_id, probability) VALUES
+(:elemental_chain_glove_pattern_ld:, 16366, 100); # Elemental Chain Glove Pattern
+
+INSERT INTO lootdrop(name) VALUES('elemental_chain_pant_pattern') # :elemental_chain_pant_pattern_ld:
+INSERT INTO lootdrop_entries(lootdrop_id, item_id, probability) VALUES
+(:elemental_chain_pant_pattern_ld:, 16367, 100); # Elemental Chain Pant Pattern
+
+INSERT INTO lootdrop(name) VALUES('elemental_chain_boot_pattern') # :elemental_chain_boot_pattern_ld:
+INSERT INTO lootdrop_entries(lootdrop_id, item_id, probability) VALUES
+(:elemental_chain_boot_pattern_ld:, 16368, 100); # Elemental Chain Boot Pattern
+
+INSERT INTO lootdrop(name) VALUES('elemental_leather_helm_pattern') # :elemental_leather_helm_pattern_ld:
+INSERT INTO lootdrop_entries(lootdrop_id, item_id, probability) VALUES
+(:elemental_leather_helm_pattern_ld:, 16376, 100); # Elemental Leather Helm Pattern
+
+INSERT INTO lootdrop(name) VALUES('elemental_leather_sleeve_pattern') # :elemental_leather_sleeve_pattern_ld:
+INSERT INTO lootdrop_entries(lootdrop_id, item_id, probability) VALUES
+(:elemental_leather_sleeve_pattern_ld:, 16378, 100); # Elemental Leather Sleeve Pattern
+
+INSERT INTO lootdrop(name) VALUES('elemental_leather_wrist_pattern') # :elemental_leather_wrist_pattern_ld:
+INSERT INTO lootdrop_entries(lootdrop_id, item_id, probability) VALUES
+(:elemental_leather_wrist_pattern_ld:, 16379, 100); # Elemental Leather Wrist Pattern
+
+INSERT INTO lootdrop(name) VALUES('elemental_leather_glove_pattern') # :elemental_leather_glove_pattern_ld:
+INSERT INTO lootdrop_entries(lootdrop_id, item_id, probability) VALUES
+(:elemental_leather_glove_pattern_ld:, 16380, 100); # Elemental Leather Glove Pattern
+
+INSERT INTO lootdrop(name) VALUES('elemental_leather_pant_pattern') # :elemental_leather_pant_pattern_ld:
+INSERT INTO lootdrop_entries(lootdrop_id, item_id, probability) VALUES
+(:elemental_leather_pant_pattern_ld:, 16381, 100); # Elemental Leather Pant Pattern
+
+INSERT INTO lootdrop(name) VALUES('elemental_leather_boot_pattern') # :elemental_leather_boot_pattern_ld:
+INSERT INTO lootdrop_entries(lootdrop_id, item_id, probability) VALUES
+(:elemental_leather_boot_pattern_ld:, 16382, 100); # Elemental Leather Boot Pattern
+
+INSERT INTO lootdrop(name) VALUES('elemental_silk_turban_pattern') # :elemental_silk_turban_pattern_ld:
+INSERT INTO lootdrop_entries(lootdrop_id, item_id, probability) VALUES
+(:elemental_silk_turban_pattern_ld:, 16383, 100); # Elemental Silk Turban Pattern
+
+INSERT INTO lootdrop(name) VALUES('elemental_silk_sleeve_pattern') # :elemental_silk_sleeve_pattern_ld:
+INSERT INTO lootdrop_entries(lootdrop_id, item_id, probability) VALUES
+(:elemental_silk_sleeve_pattern_ld:, 16385, 100); # Elemental Silk Sleeve Pattern
+
+INSERT INTO lootdrop(name) VALUES('elemental_silk_bracelet_pattern') # :elemental_silk_bracelet_pattern_ld:
+INSERT INTO lootdrop_entries(lootdrop_id, item_id, probability) VALUES
+(:elemental_silk_bracelet_pattern_ld:, 16386, 100); # Elemental Silk Bracelet Pattern
+
+INSERT INTO lootdrop(name) VALUES('elemental_silk_glove_pattern') # :elemental_silk_glove_pattern_ld:
+INSERT INTO lootdrop_entries(lootdrop_id, item_id, probability) VALUES
+(:elemental_silk_glove_pattern_ld:, 16387, 100); # Elemental Silk Glove Pattern
+
+INSERT INTO lootdrop(name) VALUES('elemental_silk_pant_pattern') # :elemental_silk_pant_pattern_ld:
+INSERT INTO lootdrop_entries(lootdrop_id, item_id, probability) VALUES
+(:elemental_silk_pant_pattern_ld:, 16388, 100); # Elemental Silk Pant Pattern
+
+INSERT INTO lootdrop(name) VALUES('elemental_silk_boot_pattern') # :elemental_silk_boot_pattern_ld:
+INSERT INTO lootdrop_entries(lootdrop_id, item_id, probability) VALUES
+(:elemental_silk_boot_pattern_ld:, 16389, 100); # Elemental Silk Boot Pattern
+
+INSERT INTO lootdrop(name) VALUES('timeless_chain_tunic_pattern') # :timeless_chain_tunic_pattern_ld:
+INSERT INTO lootdrop_entries(lootdrop_id, item_id, probability) VALUES
+(:timeless_chain_tunic_pattern_ld:, 16363, 100); # Timeless Chain Tunic Pattern
+
+INSERT INTO lootdrop(name) VALUES('timeless_breastplate_mold') # :timeless_breastplate_mold_ld:
+INSERT INTO lootdrop_entries(lootdrop_id, item_id, probability) VALUES
+(:timeless_breastplate_mold_ld:, 16375, 100); # Timeless Breastplate Mold
+
+INSERT INTO lootdrop(name) VALUES('timeless_leather_tunic_pattern') # :timeless_leather_tunic_pattern_ld:
+INSERT INTO lootdrop_entries(lootdrop_id, item_id, probability) VALUES
+(:timeless_leather_tunic_pattern_ld:, 16377, 100); # Timeless Leather Tunic Pattern
+
+INSERT INTO lootdrop(name) VALUES('timeless_silk_robe_pattern') # :timeless_silk_robe_pattern_ld:
+INSERT INTO lootdrop_entries(lootdrop_id, item_id, probability) VALUES
+(:timeless_silk_robe_pattern_ld:, 16384, 100); # Timeless Silk Robe Pattern
+
+INSERT INTO lootdrop(name) VALUES('elemental_bracer_mold') # :elemental_bracer_mold_ld:
+INSERT INTO lootdrop_entries(lootdrop_id, item_id, probability) VALUES
+(:elemental_bracer_mold_ld:, 16369, 100); # Elemental Bracer Mold
+
+INSERT INTO lootdrop(name) VALUES('elemental_boot_mold') # :elemental_boot_mold_ld:
+INSERT INTO lootdrop_entries(lootdrop_id, item_id, probability) VALUES
+(:elemental_boot_mold_ld:, 16370, 100); # Elemental Boot Mold
+
+INSERT INTO lootdrop(name) VALUES('elemental_helm_mold') # :elemental_helm_mold_ld:
+INSERT INTO lootdrop_entries(lootdrop_id, item_id, probability) VALUES
+(:elemental_helm_mold_ld:, 16371, 100); # Elemental Helm Mold
+
+INSERT INTO lootdrop(name) VALUES('elemental_vambrace_mold') # :elemental_vambrace_mold_ld:
+INSERT INTO lootdrop_entries(lootdrop_id, item_id, probability) VALUES
+(:elemental_vambrace_mold_ld:, 16372, 100); # Elemental Vambrace Mold
+
+INSERT INTO lootdrop(name) VALUES('elemental_greaves_mold') # :elemental_greaves_mold_ld:
+INSERT INTO lootdrop_entries(lootdrop_id, item_id, probability) VALUES
+(:elemental_greaves_mold_ld:, 16373, 100); # Elemental Greaves Mold
+
+INSERT INTO lootdrop(name) VALUES('elemental_gauntlet_mold') # :elemental_gauntlet_mold_ld:
+INSERT INTO lootdrop_entries(lootdrop_id, item_id, probability) VALUES
+(:elemental_gauntlet_mold_ld:, 16374, 100); # Elemental Gauntlet Mold

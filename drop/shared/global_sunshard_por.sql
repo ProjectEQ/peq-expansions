c34fcd9fad2b85799878ac92b3d19b3cc9b1fb06
Xackery Xtal <xackery@gmail.com>
2022-06-04 18:45:48 -0700

    Added powder

--- drop/shared/global_sunshard_por.sql
+++ drop/shared/global_sunshard_por.sql
@@ -4,4 +4,4 @@ INSERT INTO lootdrop_entries(lootdrop_id, item_id, chance) VALUES
 (:global_sunshard_por_ld, 33790, 100); # Sunshard Dust
 (:global_sunshard_por_ld, 33791, 100); # Sunshard Pebble
 (:global_sunshard_por_ld, 33792, 100); # Sunshard Ore
-(:global_sunshard_por_ld, 33792, 100); # Sunshard Ore
+(:global_sunshard_por_ld, 35088, 100); # Sunshard Powder
